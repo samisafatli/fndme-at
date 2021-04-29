94f3ed18b80ed4e1ec59a1a64a6fb3f92183ab34
Sami.Safatli <sami.safatli@g.globo>
2021-04-28 23:49:09 -0300

    add profile-picture component

--- src/Components/ProfilePicture/ProfilePicture.js
+++ src/Components/ProfilePicture/ProfilePicture.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './ProfilePicture.css';
 
-export const ProfilePicture = () => {
+const ProfilePicture = () => {
+  const cloudinary_url = 'https://api.cloudinary.com/v1_1/fndme/image/upload'
+  const [image, setImage] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const uploadImage = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'f1bsqjys')
+    setLoading(true)
+    const res = await fetch(
+      cloudinary_url, { method: 'POST', body: data }
+    )
+    
+    const file = await res.json()
+
+    setImage(file.secure_url)
+    setLoading(false)
+  }
+
   return (
     <div>
-      
+      <input
+        id="file"
+        type="file"
+        name="file"
+        onChange={uploadImage}
+      />
+      {loading ? ( <h3>Loading...</h3>) : ( <img src={image} alt="" />)}
+      <label htmlFor="file">Add Profile Picture</label>
     </div>
   )
 }
+
+export default ProfilePicture
